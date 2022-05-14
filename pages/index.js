fa05eafda04b19eb19262b56527b4e15451cc28e
kanaktyagi <tyagiknk.india@gmail.com>
2022-05-14 23:58:13 +0530

    Added the banner from sanity

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import HeroBanner from '../components/HeroBanner'
+import {client} from '../lib/client'
 
 
-function Home() {
+function Home({products, bannerData}) {
   return (
-    <>
-    HeroBanner
+    <div>
+    <HeroBanner 
+    heroBanner={bannerData.length && bannerData[0]}
+    />
     <div className='products-heading'>
       <h2>Best Selling Product</h2>
       <p>Speakers of many variations</p>
     </div>
     <div className='products-container'>
-    {["product1", "product2"].map (product => product)}
+    {products?.map (product => product.name)}
     </div>
-    footer
-    </>
+    <Footer/>
+    </div>
   )
+}
+export  async function  getServerSideProps(){
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query);
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+    return {
+      props:{
+        products, bannerData
+      }
+    }
 }
 
 export default Home
